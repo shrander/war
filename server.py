bfe05bfb0e524cf0659d70d69530c482da48f710
Nick <nhnatiw@gmail.com>
2013-04-01 11:39:58 -0300

    Update server.py
    first update.  still not functional.

--- server.py
+++ server.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python
+
+import socket
+import sys
+
+class war_server:
+  def __init__(self):
+    self.server_address = ('localhost', 1337)
+    
+  def parse_args(self, args):
+    i=0
+    while(i<len(args)):
+      if i == '-h':
+        i+=1
+        self.server_address[0] = args[i]
+      else:
+        self.usage()
+        
+  def main(self, args):
+    self.parse_args(args)
+  
+if __name__ == 'main':
+  w = war_server()
+  w.main()
